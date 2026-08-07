18b064f9370ba56de667a97a11ea2f32afa0a1bb
Aluno <Aluno@DESKTOP-O31M9M7>
2026-08-07 11:53:23 -0300

    Adicionar arquivos de projeto.

---
+++ CombustivelApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace CombustivelApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            // Verifica se todos os campos foram preenchidos
+            if (string.IsNullOrWhiteSpace(txtAlcool.Text) ||
+                string.IsNullOrWhiteSpace(txtGasolina.Text) ||
+                string.IsNullOrWhiteSpace(txtKmInicial.Text) ||
+                string.IsNullOrWhiteSpace(txtKmFinal.Text) ||
+                string.IsNullOrWhiteSpace(txtLitros.Text) ||
+                string.IsNullOrWhiteSpace(txtConsumoAlcool.Text) ||
+                string.IsNullOrWhiteSpace(txtConsumoGasolina.Text))
+            {
+                DisplayAlert("Erro", "Preencha todos os campos.", "OK");
+                return;
+            }
+
+            }
+    }
+
+}
---
+++ CombustivelApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CombustivelApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
